83b8857b3223f8c43ad3235b832a72ef3989293c
Samuel <s.looi2@outlook.com>
2022-12-17 22:02:43 +1300

    Create position2d component

---
+++ src/ecs/components/Position2DComponent.ts
@@ -0,0 +1,23 @@
+import { Component } from "../../packages/ecs";
+
+export default class Position2DComponent extends Component {
+	private _x: number;
+	public get x(): number {
+		return this._x;
+	}
+	public set x(value: number) {
+		this._x = value;
+	}
+	private _y: number;
+	public get y(): number {
+		return this._y;
+	}
+	public set y(value: number) {
+		this._y = value;
+	}
+	constructor(x: number, y: number) {
+		super();
+		this._x = x;
+		this._y = y;
+	}
+}
