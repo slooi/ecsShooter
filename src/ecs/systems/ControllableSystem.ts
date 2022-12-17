f743bc4a899686f5d3b07a128959c8b712eb4ce6
Samuel <s.looi2@outlook.com>
2022-12-17 23:05:47 +1300

    Create controllable system

---
+++ src/ecs/systems/ControllableSystem.ts
@@ -0,0 +1,31 @@
+import InputManager from "../../client/InputManager";
+import { ECSManager, System } from "../../packages/ecs";
+import ControllableComponent from "../components/ControllableComponent";
+import Position2DComponent from "../components/Position2DComponent";
+
+export default class ControllableSystem extends System {
+	inputManager: InputManager;
+	constructor(inputManager: InputManager) {
+		super();
+		this.inputManager = inputManager;
+	}
+	update(ecsManager: ECSManager): void {
+		const entityComponentTuples = ecsManager.view(ControllableComponent, Position2DComponent);
+		entityComponentTuples.forEach((entityComponentTuple) => {
+			const positionComponent = entityComponentTuple[1].get(Position2DComponent);
+
+			if (this.inputManager.keysDown["KeyW"]) {
+				positionComponent.y -= 5;
+			}
+			if (this.inputManager.keysDown["KeyS"]) {
+				positionComponent.y += 5;
+			}
+			if (this.inputManager.keysDown["KeyA"]) {
+				positionComponent.x -= 5;
+			}
+			if (this.inputManager.keysDown["KeyD"]) {
+				positionComponent.x += 5;
+			}
+		});
+	}
+}
