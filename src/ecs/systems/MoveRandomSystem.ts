772c04f4b6541838741057d4a9763becbec42020
Samuel <s.looi2@outlook.com>
2022-12-18 21:27:45 +1300

    Add MoveRandomComponent as parameter

--- src/ecs/systems/MoveRandomSystem.ts
+++ src/ecs/systems/MoveRandomSystem.ts
@@ -1,9 +1,10 @@
 import { ECSManager, System } from "../../packages/ecs";
+import MoveRandomComponent from "../components/MoveRandomComponent";
 import Position2DComponent from "../components/Position2DComponent";
 
 export default class MoveRandomSystem extends System {
 	update(ecsManager: ECSManager): void {
-		const entityComponentObjTuples = ecsManager.view(Position2DComponent);
+		const entityComponentObjTuples = ecsManager.view(Position2DComponent, MoveRandomComponent);
 		entityComponentObjTuples.forEach((entityComponentObjTuple) => {
 			const position2DComponent = entityComponentObjTuple[1].get(Position2DComponent);
 			position2DComponent.x += Math.random() * 10 - 5;
