9cc9c1ed8f60f2cbbe3d9a97484a368d9d65801e
Samuel <s.looi2@outlook.com>
2022-12-18 21:26:54 +1300

    Add ___type so can't get mixed with Components

--- src/packages/ecs/System.ts
+++ src/packages/ecs/System.ts
@@ -1,6 +1,7 @@
 import { ECSManager } from "./ECSManager";
 
 export abstract class System {
+	readonly ___type = "System"; // Need this or else I could accidentally specify a system as a component
 	constructor() {}
 	abstract update(ecsManager: ECSManager): void;
 }
