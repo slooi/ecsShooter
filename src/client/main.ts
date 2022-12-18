9794708c1971ab88ff2adbe9649d380bdaee00ec
Samuel <s.looi2@outlook.com>
2022-12-18 17:26:51 +1300

    Extract code into game class

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,9 +1,4 @@
-import ControllableComponent from "../ecs/components/ControllableComponent";
-import Position2DComponent from "../ecs/components/Position2DComponent";
-import ControllableSystem from "../ecs/systems/ControllableSystem";
-import RendererSystem from "../ecs/systems/RendererSystem";
-import { ECSManager } from "../packages/ecs";
-import InputManager from "./InputManager";
+import Game from "./Game";
 
 // WEBSOCKET
 const ws = new WebSocket("ws://localhost:8080/"); //!@#!@#!@# CHANGE LATER
@@ -15,26 +10,4 @@ ws.addEventListener("message", (e) => {
 	console.log(e.data);
 });
 
-//
-const canvas = document.createElement("canvas");
-canvas.width = 500;
-canvas.height = 500;
-document.body.appendChild(canvas);
-
-// SETUP
-const inputManager = new InputManager();
-const ecsManager = new ECSManager();
-
-// ADD ENTITIES
-ecsManager.addEntity(new Position2DComponent(10, 15), new ControllableComponent());
-ecsManager.addEntity(new Position2DComponent(10, 150));
-
-// ADD SYSTEMS
-ecsManager.addSystems(new ControllableSystem(inputManager), new RendererSystem(canvas));
-
-function updateLoop() {
-	ecsManager.updateSystems();
-
-	requestAnimationFrame(updateLoop);
-}
-updateLoop();
+const game = new Game();
